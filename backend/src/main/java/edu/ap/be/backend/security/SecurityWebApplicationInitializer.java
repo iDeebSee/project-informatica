90e8d2a41e6567789f08710207b42ef64809944c
Abdelmajid Amiri <58814178+iDeebSee@users.noreply.github.com>
2022-02-19 22:17:17 +0100

    Authorisatie

---
+++ backend/src/main/java/edu/ap/be/backend/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package edu.ap.be.backend.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}

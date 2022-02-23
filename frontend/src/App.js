51ddc913e550301b7b6796e92a2d0edb12d2cdf6
Abdelmajid Amiri <58814178+iDeebSee@users.noreply.github.com>
2022-02-23 15:28:13 +0100

    services voor kredietaanvragen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,16 @@
 import './App.css';
 import * as React from 'react';
-import Home from './views/Home';
-import Kredietaanvragen from './klantViews/KredietAanvragen';
+import Routing from "./routing/Routing";
 import Navbar from './components/Navbar';
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
-
-
 
 
 export default function App() {
-  
-  return(
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-    <Route  path={"/"} element={<Home/>}/>
-    
-    <Route  path={"/list"} element={<Kredietaanvragen/>}/>
 
-    
-
-    </Routes>
-   
-    
-    </BrowserRouter>
-);
+  return (
+    <div>
+      <Navbar />
+      <Routing></Routing>
+    </div>
+  );
 };
 

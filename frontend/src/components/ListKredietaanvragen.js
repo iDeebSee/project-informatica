ce7c30d0df9ea0460f11a8ff04a09d0c5bc8ef3b
JarnoDeSmedt <55498374+JarnoDeSmedt@users.noreply.github.com>
2022-02-27 20:42:42 +0100

    frontend nieuwe kredietaanvraag popup scherm verder aangevuld

--- frontend/src/components/ListKredietaanvragen.js
+++ frontend/src/components/ListKredietaanvragen.js
@@ -1,18 +1,27 @@
-import * as React from 'react';
-import Link from '@mui/material/Link';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { Container } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import { borderRight } from '@mui/system';
-import { Modal, Typography, Box, TextField, Slider, Paper } from '@mui/material'
-
-
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { borderRight } from "@mui/system";
+import {
+  Modal,
+  Typography,
+  Box,
+  TextField,
+  Slider,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  TextareaAutosize,
+} from "@mui/material";
 
 // Generate Order Data
 function createData(id, date, name, shipTo, paymentMethod, amount) {
@@ -22,85 +31,91 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
 const rows = [
   createData(
     0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
+    "16 Mar, 2019",
+    "Elvis Presley",
+    "Tupelo, MS",
+    "VISA ⠀•••• 3719",
+    312.44
   ),
   createData(
     1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
+    "16 Mar, 2019",
+    "Paul McCartney",
+    "London, UK",
+    "VISA ⠀•••• 2574",
+    866.99
   ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+  createData(
+    2,
+    "16 Mar, 2019",
+    "Tom Scholz",
+    "Boston, MA",
+    "MC ⠀•••• 1253",
+    100.81
+  ),
   createData(
     3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
+    "16 Mar, 2019",
+    "Michael Jackson",
+    "Gary, IN",
+    "AMEX ⠀•••• 2000",
+    654.39
   ),
   createData(
     4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
+    "15 Mar, 2019",
+    "Bruce Springsteen",
+    "Long Branch, NJ",
+    "VISA ⠀•••• 5919",
+    212.79
   ),
 ];
 
 const marks = [
   {
     value: 1,
-    label: '1 maand',
+    label: "1 maand",
   },
   {
     value: 12,
-    label: '1 jaar',
+    label: "1 jaar",
   },
 ];
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 1200,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 function valuetext(value) {
-  if (value == 1)
-    return `${value} maand`;
-  else if (value == 12)
-    return `${value} Jaar`;
-  else
-    return `${value} maanden`;
-}
+  if (value == 1) return `${value} maand`;
+  else if (value == 12) return `${value} Jaar`;
+  else return `${value} maanden`;
 
+}
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
 export default function ListKredietaanvragen() {
-
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [reden, setReden] = React.useState("");
 
+  const handleChange = (event) => {
+    setReden(event.target.value);
+  };
 
   const Popup = (
-
     <div>
       <Modal
         open={open}
@@ -113,27 +128,76 @@ export default function ListKredietaanvragen() {
             Nieuwe kredietaanvraag indienen
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            hier komt de body voor het aanvragen van kredieten
+            Vul hier de gegevens in voor uw kredietaanvraag. Druk op versturen
+            wanneer u klaar bent.
             <Box
               component="form"
               sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
+                "& > :not(style)": { m: 1, width: "25ch" },
               }}
               noValidate
               autoComplete="off"
             >
-              <Grid container spacing={2} style={{width: "100%"}}>
-                <Grid item xs={6} md={4}>
-                  <TextField id="outlined-basic" label="Verantwoording" variant="outlined" />
+              <Grid container spacing={2} style={{ width: "95%" }}>
+                <Grid item xs={6} md={12}>
+                  <TextField
+                    id="naam"
+                    required="true"
+                    label="Naam van de kredietaanvraag"
+                    variant="outlined"
+                  />
                 </Grid>
-                 {/* nog combobox van maken  */}
-                <Grid item xs={6} md={4}>
-                  <TextField id="filled-basic" label="Zelf gefinancierd (€)" variant="outlined" />
+                <Grid item xs={6} md={12}>
+                  <TextField
+                    id="zelfgefinancierd"
+                    required="true"
+                    label="Zelf gefinancierd (€)"
+                    variant="outlined"
+                  />
                 </Grid>
-                <Grid item xs={6} md={4}>
-                  <TextField id="standard-basic" label="Totaalbedrag (€)" variant="outlined" />
+                <Grid item xs={6} md={12}>
+                  <TextField
+                    id="totaalbedrag"
+                    required="true"
+                    label="Totaalbedrag (€)"
+                    variant="outlined"
+                  />
                 </Grid>
-                <Grid item xs={6} md={8}>
+                <Grid item xs={6} md={12}>
+                  <FormControl>
+                    <InputLabel id="demo-simple-select-label">
+                      Categorie
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      style={{width : 300}}
+                      value={reden}
+                      label="Reden"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"meubilair en machines"}>
+                      meubilair en machines
+                      </MenuItem>
+                      <MenuItem value={"rollend materieel(vb laptop)"}>
+                      rollend materieel(vb laptop)
+                      </MenuItem>
+                      <MenuItem value={"klein materieel(vb gsm)"}>
+                      klein materieel(vb gsm)
+                      </MenuItem>
+                      <MenuItem value={"kantoor"}>
+                      kantoor
+                      </MenuItem>
+                      <MenuItem value={"gebouwen"}>
+                      gebouwen
+                      </MenuItem>
+                      <MenuItem value={"industriele gebouwen"}>
+                      industriële gebouwen
+                      </MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={6} md={12}>
                   <Slider
                     aria-label="Looptijd"
                     defaultValue={6}
@@ -141,9 +205,24 @@ export default function ListKredietaanvragen() {
                     valueLabelDisplay="on"
                     step={1}
                     marks={marks}
+                    // nog een if statement dat checkt welke categorie
+                    // voor 
+                    // meubilair en machines (tot 10j), rollend materieel(vb laptop) (tot 5j), klein materieel(vb gsm) (tot 3j)
+                    // kantoor (tot 33j), gebouwen (tot 33j), industriele gebouwen (tot 20j)
                     min={1}
                     max={12}
+                  /> {console.log(Slider.getAriaValueText)}
+                </Grid>
+                <Grid item xs={6} md={12}>
+                  <TextField
+                    multiline="true"
+                    minRows={5}
+                    placeholder="Typ hier de verantwoording voor uw kredietaanvraag..."
+                    style={{ width: "100%" }}
                   />
+                </Grid>
+                <Grid item xs={6} md={12}>
+                <Button variant="contained">Verstuur aanvraag</Button>
                 </Grid>
               </Grid>
             </Box>
@@ -151,16 +230,23 @@ export default function ListKredietaanvragen() {
         </Box>
       </Modal>
     </div>
-  )
+  );
 
   return (
-    <Container maxWidth="lg" style={{ position: 'relative', marginTop: 20 }}>
+    <Container maxWidth="lg" style={{ position: "relative", marginTop: 20 }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <ButtonGroup style={{ float: 'right', marginRight: 15 }} variant="contained" aria-label="outlined primary button group">
-            <Button style={{ align: borderRight }} onClick={handleOpen}>create new</Button>
+          <ButtonGroup
+            style={{ float: "right", marginRight: 15 }}
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button style={{ align: borderRight }} onClick={handleOpen}>
+              create new
+            </Button>
             {Popup}
-          </ButtonGroup>  </Grid>
+          </ButtonGroup>{" "}
+        </Grid>
         <Grid item xs={12}>
           <React.Fragment>
             <Table size="small">
@@ -187,7 +273,10 @@ export default function ListKredietaanvragen() {
                     <TableCell align="right">nog in te vullen</TableCell>
                     <TableCell align="right">nog in te vullen</TableCell>
                     <TableCell align="right">
-                      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                      <ButtonGroup
+                        variant="contained"
+                        aria-label="outlined primary button group"
+                      >
                         <Button>edit </Button>
                         <Button>delete </Button>
                       </ButtonGroup>
@@ -196,7 +285,6 @@ export default function ListKredietaanvragen() {
                 ))}
               </TableBody>
             </Table>
-
           </React.Fragment>
         </Grid>
       </Grid>

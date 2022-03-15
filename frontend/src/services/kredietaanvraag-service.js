eb4fcf4ae4d6578d3f61b0e7ea58c3cc38a55ed3
unknown <zakaria.elmossaoui@outlook.com>
2022-03-15 20:25:37 +0100

    create kredietaanvraag volldig afgewerkt

--- frontend/src/services/kredietaanvraag-service.js
+++ frontend/src/services/kredietaanvraag-service.js
@@ -1,24 +1,56 @@
-import http from "./https-common";
-const baseUrl = '/kredietaanvragen';
+import axios from 'axios';
+import httpsCommon from './https-common';
 
-class KredietAanvraagService {
-    getAll() {
-        return http.get(`${baseUrl}/`);
-    }
+// import http from "./https-common";
+const baseUrl = 'http://localhost:8080/kredietaanvragen';
 
-    get(id) {
-        return http.get(`${baseUrl}/${id}`);
-    }
+ class KredietAanvraagService {
+    // getAll() {
+    //     return http.get(`${baseUrl}/`);
+    // }
 
-    create() {
-        return http.post(`${baseUrl}/`);
-    }
+    // get(id) {
+    //     return http.get(`${baseUrl}/${id}`);
+    // }
 
-    update(id) {
-        return http.put(`${baseUrl}/${id}`);
-    }
+    
+    create(lening,termijn,naam , verantwoording,eigenvermogen,categorie) {
 
-    delete(id) {
-        return http.delete(`${baseUrl}/${id}`);
+            let formdata= new FormData();
+            formdata.append('lening',lening);
+            formdata.append('looptijd',termijn);
+            //formdata.append('file',bestand);
+            formdata.append('naam',naam);
+            formdata.append('verantwoording',verantwoording);
+            formdata.append('zelfgefinancieerd',eigenvermogen);
+            formdata.append('categorie',categorie)
+            // if(bestand==undefined)
+            // {
+            //     bestand.array.forEach(bestand => {
+            //         formdata.append('files',bestand);
+                    
+            //     });
+            // }
+
+            const config= 
+            {headers: 
+                { "Content-Type": "multipart/form-data"  }
+                
+            }
+            return httpsCommon.post(`${baseUrl}`,formdata, config
+             );
+           
+            
+            
     }
+   
+
+    // update(id) {
+    //     return http.put(`${baseUrl}/${id}`);
+    // }
+
+    // delete(id) {
+    //     return http.delete(`${baseUrl}/${id}`);
+    // }
 }
+export default new KredietAanvraagService();

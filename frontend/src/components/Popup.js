21b43c60a17570fea5c035c2ff96eb8b36f42285
JarnoDeSmedt <jarno.desmedt@live.be>
2022-03-16 13:34:09 +0100

    afhankelijke termijnen op basis van de reden van krediet

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -15,84 +15,80 @@ import {
   TextareaAutosize,
 } from "@mui/material";
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "90%",
-    height: "80%",
-    overflow: "scroll",
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  height: "80%",
+  overflow: "scroll",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
 };
 
- export const  Popup= React.forwardRef((props, ref)=> {
-    
-    const [open, setOpen] = React.useState(false);
-    const handleClose = () => setOpen(false);
-    const [termijn, setTermijn]= React.useState(0)
-    const [zelfGefinancierd,setZelfGefinancieerd ]= React.useState(0)
-    const [totaalbedrag, setTotaalbedrag]= React.useState(0)
-    const [naam, setNaam]= React.useState("")
-    const [categorie, setCategorie]= React.useState("")
-    const [verantwoording, setVerantwoording]= React.useState("")
-    const [file,setFile]=React.useState("")
-    
+export const Popup = React.forwardRef((props, ref) => {
+
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+  const [termijn, setTermijn] = React.useState(0)
+  const [zelfGefinancierd, setZelfGefinancieerd] = React.useState(0)
+  const [totaalbedrag, setTotaalbedrag] = React.useState(0)
+  const [naam, setNaam] = React.useState("")
+  const [categorie, setCategorie] = React.useState("")
+  const [verantwoording, setVerantwoording] = React.useState("")
+  const [file, setFile] = React.useState("")
 
 
-    const handleChange = (event) => {
-      setCategorie(event.target.value)
 
-    };
-    
-    React.useImperativeHandle(ref, () => ({
-         handleOpen () {
-          setOpen(true);
-        
-        }
-      }));
+  const handleChange = (event) => {
+    setCategorie(event.target.value)
 
-      function valuetext(value) {
-        if (value == 1) return `${value} maand`;
-        else if (value == 12) return `${value} Jaar`;
-        else return `${value} maanden`;
-      
-      }
-      
-     function handleSubmit(e){
-      e.preventDefault(); 
-      KredietAanvraagService.create(
-          totaalbedrag,
-          termijn,
-          naam,
-          verantwoording,
-          zelfGefinancierd,
-          categorie
-          
-      ).then(response => {
-        console.log(response.data)
-        window.location.reload();
-      })
+  };
+
+  React.useImperativeHandle(ref, () => ({
+    handleOpen() {
+      setOpen(true);
+
+    }
+  }));
+
+  function valuetext(value) {
+    if (value == 1) return `${value} maand`;
+    else if (value == 12) return `${value} Jaar`;
+    else return `${value} maanden`;
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    KredietAanvraagService.create(
+      totaalbedrag,
+      termijn,
+      naam,
+      verantwoording,
+      zelfGefinancierd,
+      categorie
+
+    ).then(response => {
+      console.log(response.data)
+      window.location.reload();
+    })
 
 
-     }
+  }
 
-      const marks = [
-        {
-          value: 1,
-          label: "1 maand",
-        },
-        {
-          value: 12,
-          label: "1 jaar",
-        },
-      ];
-            
-   
+  const marks = [
+    {
+      value: "min",
+      label: "Jaar",
+    }
+  ];
+
+
   return (
-    
+
     <Modal
       open={open}
       onClose={handleClose}
@@ -107,8 +103,8 @@ const style = {
           Vul hier de gegevens in voor uw kredietaanvraag. Druk op versturen
           wanneer u klaar bent.
           <Box
-          onSubmit={handleSubmit}
-          
+            onSubmit={handleSubmit}
+
             component="form"
             sx={{
               "& > :not(style)": { m: 1, width: "25ch" },
@@ -134,7 +130,7 @@ const style = {
                   required="true"
                   label="Zelf gefinancierd (€)"
                   variant="outlined"
-                  style={{ width: "100%" }}µ
+                  style={{ width: "100%" }} µ
                   onChange={(e) => setZelfGefinancieerd(e.target.value)}
 
                 />
@@ -150,7 +146,7 @@ const style = {
                   value={totaalbedrag}
                 />
               </Grid>
-              <Grid item xs={12} md={3} style= {{ maxWidth: "100%" }}>
+              <Grid item xs={12} md={3} style={{ maxWidth: "100%" }}>
                 <FormControl  >
                   <InputLabel id="demo-simple-select-label">
                     Categorie
@@ -158,49 +154,75 @@ const style = {
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    style={{width : 300}}
+                    style={{ width: 300 }}
                     value={categorie}
                     label="Reden"
                     onChange={handleChange}
                   >
                     <MenuItem value={"GEBOUWEN"}>
-                    meubilair en machines
+                      gebouwen
                     </MenuItem>
                     <MenuItem value={"ROLLENDMATERIEEL"}>
-                    rollend materieel(vb laptop)
+                      rollend materieel
                     </MenuItem>
                     <MenuItem value={"KLEINMATERIEEL"}>
-                    klein materieel(vb gsm)
+                      klein materieel(vb gsm)
                     </MenuItem>
                     <MenuItem value={"KANTOOR"}>
-                    kantoor
+                      kantoor
                     </MenuItem>
                     <MenuItem value={"INDUSTRIEELEGEBOUWEN"}>
-                    gebouwen
+                      industriële gebouwen
                     </MenuItem>
-                    <MenuItem value={"MEUBELAIRENMACHINES"}>
-                    industriële gebouwen
+                    <MenuItem value={"MEUBILAIRENMACHINES"}>
+                      meubilair en machines
                     </MenuItem>
                   </Select>
                 </FormControl>
               </Grid>
-              
-              <Grid item xs={12} md={12} style={{marginTop: "5%"}}>
+
+              <Grid item xs={12} md={12} style={{ marginTop: "5%" }}>
                 <Slider
                   aria-label="Looptijd"
-                  defaultValue={6}
                   getAriaValueText={valuetext}
                   valueLabelDisplay="on"
                   step={1}
                   marks={marks}
                   onChange={(e) => setTermijn(e.target.value)}
                   value={termijn}
-                  // nog een if statement dat checkt welke categorie
-                  // voor 
-                  // meubilair en machines (tot 10j), rollend materieel(vb laptop) (tot 5j), klein materieel(vb gsm) (tot 3j)
-                  // kantoor (tot 33j), gebouwen (tot 33j), industriele gebouwen (tot 20j)
-                  min={1}
-                  max={12}
+                  min={(() => {
+                    switch (categorie) {
+                      case "GEBOUWEN":
+                        return 5;
+                      case "ROLLENDMATERIEEL":
+                        return 2;
+                      case "KLEINMATERIEEL":
+                        return 1;//?
+                      case "KANTOOR":
+                        return 1;//?
+                      case "INDUSTRIEELEGEBOUWEN":
+                        return 1;//?
+                      case "MEUBILAIRENMACHINES":
+                        return 1;//?
+                    }
+                  })()}
+                  max={(() => {
+
+                    switch (categorie) {
+                      case "GEBOUWEN":
+                        return 33;
+                      case "ROLLENDMATERIEEL":
+                        return 5;
+                      case "KLEINMATERIEEL":
+                        return 3;
+                      case "KANTOOR":
+                        return 33;
+                      case "INDUSTRIEELEGEBOUWEN":
+                        return 20;
+                      case "MEUBILAIRENMACHINES":
+                        return 10;
+                    }
+                  })()}
                 /> {console.log(Slider.getAriaValueText)}
               </Grid>
               <Grid item xs={12} md={12}>
@@ -214,7 +236,7 @@ const style = {
                 />
               </Grid>
               <Grid item xs={12} md={12}>
-              <Button variant="contained" type="submit">verstuur </Button>
+                <Button variant="contained" type="submit">verstuur </Button>
 
 
               </Grid>
@@ -223,6 +245,6 @@ const style = {
         </Typography>
       </Box>
     </Modal>
-  
-   );
-} )
+
+  );
+})

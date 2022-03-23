5585ab967cbe318362d34b13a4085eb7502ce88f
Abdelmajid Amiri <58814178+iDeebSee@users.noreply.github.com>
2022-03-23 14:10:53 +0100

    pdf gefixt

--- frontend/src/components/FeedbackDocumentPDF.js
+++ frontend/src/components/FeedbackDocumentPDF.js
@@ -1,6 +1,5 @@
 //npm install react-pdf
 import React, { useState } from 'react';
-import { Document, Page,pdfjs } from 'react-pdf';
 
 
 //PDFjs worker from an external cdn
@@ -9,24 +8,11 @@ const url =
 
 export default function FeedbackDocumentPDF() {
 	
-	pdfjs.GlobalWorkerOptions.workerSrc =
-	`//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-	const [numPages, setNumPages] = useState(null);
-	const [pageNumber, setPageNumber] = useState(1);
-
-	function onDocumentLoadSuccess({ numPages }) {
-	setNumPages(numPages);
-	setPageNumber(1);
-}
+	
 return (
 	<>
 	<div className="main">
-	<Document
-		file={url}
-		onLoadSuccess={onDocumentLoadSuccess}
-		>
-		<Page pageNumber={pageNumber} />
-	</Document>
+	
 	</div>
 	</>
 );
--- frontend/src/klantViews/DetailAanvraag.js
+++ frontend/src/klantViews/DetailAanvraag.js
@@ -12,99 +12,132 @@ import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
-
-
-
+import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+import kredietaanvraagService from "../services/kredietaanvraag-service";
+import EventBus from "../common/eventBus"
 
 
 
 export default function Detailaanvraag() {
+
+    const [image, setImage] = React.useState()
+    const [krediet, setKredieten] = React.useState()
+
+    function printDocument() {
+        const input = document.getElementById('divToPrint');
+        html2canvas(input)
+            .then((canvas) => {
+                const imgData = canvas.toDataURL('image/png');
+                const pdf = new jsPDF();
+                pdf.addImage(imgData, 'PNG', 0, 0);
+                // pdf.output('dataurlnewwindow');
+                pdf.save("download.pdf");
+            });
+    }
+
+
+    React.useEffect(() => {
+
+        kredietaanvraagService.getAll().then((response) => {
+            console.log("data", response.data)
+            setKredieten(response.data)
+
+        }).then(error => {
+            if (error.response && error.response.status === 401) {
+                EventBus.dispatch("logout");
+            }
+        })
+        const input = document.getElementById('divToPrint');
+        html2canvas(input).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            setImage(imgData)
+        })
+    }, [])
+
     return (
-     
-                 <div >
-            
-           
-                    
-                        <Grid container spacing={0} style={{top: 20,height:500, position: 'relative',left: 5}}>
-                           
-                            <Grid item xs={2}  >
-                            <Box sx={{  maxWidth: 200, bgcolor: 'Background.paper' }}>
-                            
-                                    <List>
-                                    <ListItem  disablePadding>
-                                        <ListItemText primary="Persoonsgegevens" />
-                                    </ListItem>
-                                    </List>
-                                <Divider />
-                                    <List>
-                                    <ListItem >
-                                        <ListItemText primary="naam" />
-                                    </ListItem>
-                                    <ListItem >
-                                        <ListItemText primary="telNr..." />
-                                    </ListItem>
-                                    </List>
-                                
-                                </Box>
-                              </Grid>
-                              
-                            <Grid item xs={2} >
-                            <Box sx={{ width: '50%', maxWidth: 200, bgcolor: 'background.paper' }}>
-                            
-                                    <List>
-                                    <ListItem  disablePadding>
-                                       
-                                        <ListItemText primary="fnancieel" />
-                                    </ListItem>
-                                    </List>
-                                <Divider />
-                                    <List>
-                                    <ListItem >
-                                        
-                                        <ListItemText primary="zelf gefinancieerd" />
-                                    </ListItem>
-                                    <ListItem >
-                                        <ListItemText primary="status" />
-                                    </ListItem>
-                                    </List>
-                                
-                                </Box>
-                              </Grid>
-                              <Grid item xs={8}>
-                            <Box sx={{ width: '50%', maxWidth: 200, bgcolor: 'background.paper' }}>
-                            
-                                    <List>
-                                    <ListItem disablePadding>
-                                        
-                                       
-                                        <ListItemText primary="feedback document" />
-                                    </ListItem>
-                                    </List>
-                                <Divider />
-                                    <List>
-                                    <ListItem >
-                                        <ListItemText primary="hier komt feedbackdocument" />
-                                    </ListItem>
-                                    </List>
-                                
-                                </Box>
-                              </Grid>
+        <div >
+            <Grid container spacing={0} style={{ top: 20, height: 500, position: 'relative', left: 0 }}>
+                <Grid item id="divToPrint" style={{
+                    display: "inline-flex", width: '180mm',
+                    //minHeight: '297mm',
+                    padding: 10, fontSize: "10pt"
+                }} xs={12} md={6}>
+                    <Grid item xs={4}  >
+                        <Box sx={{ maxWidth: 200, bgcolor: 'Background.paper' }}>
+
+                            <List>
+                                <ListItem disablePadding>
+                                    <ListItemText primary="Persoonsgegevens" />
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem >
+                                    <ListItemText primary="naam" />
+                                </ListItem>
+                                <ListItem >
+                                    <ListItemText primary="telNr..." />
+                                </ListItem>
+                            </List>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={4} >
+                        <Box sx={{ maxWidth: 200, bgcolor: 'background.paper' }}>
+                            <List>
+                                <ListItem disablePadding>
+
+                                    <ListItemText primary="fnancieel" />
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem >
+                                    <ListItemText primary="zelf gefinancieerd" />
+                                </ListItem>
+                                <ListItem >
+                                    <ListItemText primary="status" />
+                                </ListItem>
+                            </List>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Box sx={{ maxWidth: 200, bgcolor: 'background.paper' }}>
+                            <List>
+                                <ListItem disablePadding>
+                                    <ListItemText primary="feedback document" />
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem >
+                                    <ListItemText primary="hier komt feedbackdocument" />
+                                </ListItem>
+                            </List>
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={6}>
+
+                    <img src={image} style={{ width: "700px", border: "1px solid grey" }} />
+
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <ButtonGroup variant="contained" aria-label="outlined primary button group" style={{ position: 'relative' }}>
+                        <Button variant="contained">contract uploaden</Button>
+                        <Button variant="contained">contract tekenen</Button>
+                        <Button variant="contained">contract bekijken</Button>
+                        <Button onClick={printDocument}>Druk af</Button>
+                    </ButtonGroup>
+                </Grid>
+
+            </Grid>
 
 
 
 
-                            <Grid item xs={12} md={4}>
-                            <ButtonGroup variant="contained" aria-label="outlined primary button group" style={{position: 'relative'}}>
-                                <Button variant="contained">contract uploaden</Button>
-                                <Button variant="contained">contract tekenen</Button>
-                                <Button variant="contained">contract bekijken</Button>
-                                </ButtonGroup>
-                            </Grid>
-                           
-                        </Grid>
-                   
-    </div> 
-           
+        </div>
+
     );
 };
 
